eb71042e4ba2763816f843556bc9a41c96687156
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-10-21 03:04:20 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow(arr)
-  return "R: #{arr[0]}, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+  return "R: #{arr[0]}, O: #{arr[1]}, Y: #{arr[2]}, G: #{arr[3]}, B: #{arr[4]}, I: #{arr[5]}, V: #{arr[6]}"
 end
